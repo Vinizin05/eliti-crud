83a4089575ae018886b7375abac518a0400d6c72
Vinicius Rodrigues Varela <rodigues.vv@gmail.com>
2023-04-10 15:22:05 -0300

    adicionando comentários

--- JS/script.js
+++ JS/script.js
@@ -1,10 +1,12 @@
 const inputs = document.querySelectorAll(".input");
 
+//adiciona foco ao clicar no input
 function addcl() {
   let parent = this.parentNode.parentNode;
   parent.classList.add("focus");
 }
 
+//remove o foco ao clicar em qualquer lugar da tela
 function remcl() {
   let parent = this.parentNode.parentNode;
   if (this.value == "") {
@@ -12,11 +14,15 @@ function remcl() {
   }
 }
 
+//adiciona evento nos inputs
 inputs.forEach((input) => {
   input.addEventListener("focus", addcl);
   input.addEventListener("blur", remcl);
 });
 
+// redirecionamento de pagina
 function entrar() {
   window.location.href = "/html/usuario.html";
 }
+
+
