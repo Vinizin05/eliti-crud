22d3ef483ae1bf5b7d8107908eedc59810c7674c
Vinicius Rodrigues Varela <rodigues.vv@gmail.com>
2023-04-12 19:02:17 -0300

    iniciado JS

--- JS/script.js
+++ JS/script.js
@@ -1,28 +1,42 @@
 const inputs = document.querySelectorAll(".input");
 
 //adiciona foco ao clicar no input
-function addcl() {
+function addFocus() {
   let parent = this.parentNode.parentNode;
   parent.classList.add("focus");
 }
 
 //remove o foco ao clicar em qualquer lugar da tela
-function remcl() {
+function remFocus() {
   let parent = this.parentNode.parentNode;
   if (this.value == "") {
     parent.classList.remove("focus");
   }
 }
 
-//adiciona evento nos inputs
+//adiciona evento nos inputs    /*forEach =>  é um método de loop que é usado em arrays e outros tipos de objetos iteráveis*/
 inputs.forEach((input) => {
-  input.addEventListener("focus", addcl);
-  input.addEventListener("blur", remcl);
+  input.addEventListener("focus", addFocus);
+  input.addEventListener("blur", remFocus);
 });
 
 // redirecionamento de pagina
 function entrar() {
-  window.location.href = "/html/usuario.html";
+  var email = document.querySelector(".gmail");
+  var senha = document.querySelector(".password");
+
+  if (email.value == "admin@gmail.com" && senha.value == "admin") { 
+    localStorage.setItem("acesso", true);
+
+    window.location.href = "/html/usuario.html";
+
+  } else {
+    alert("Email ou Senha nao cadastrados")
+  }
+  
+
+    
+
 }
-
-
+  
+  
