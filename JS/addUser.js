df401587049d8ded9a879f6a294b6d05119f6bd8
Vinicius Rodrigues Varela <rodigues.vv@gmail.com>
2023-04-20 14:40:04 -0300

    add localstorage

--- JS/addUser.js
+++ JS/addUser.js
@@ -1,22 +1,34 @@
-const nome = document.querySelector("#nome")
-const email = document.querySelector("#email")
-const senha = document.querySelector("#senha")
+// const nome = document.querySelector("#nome")
+// const email = document.querySelector("#email")
+// const senha = document.querySelector("#senha")
 
 
 function cadastrar(){
     const nome = document.querySelector("#nome")
     const email = document.querySelector("#email")
     const senha = document.querySelector("#senha")
-    let listaUser = JSON.parse(localStorage.getItem('listaUser')) || []
+    let listaUserStorage = JSON.parse(localStorage.getItem('listaUser'))
 
-    listaUser.push({
-        nome: nome.value ,
-        email: email.value ,
-        senha: senha.value
+    let novoUsuario;
+
+    if(listaUserStorage){
+        console.log("entrou no if")
+        novoUsuario = [...listaUserStorage, {
+            'nome': nome.value,
+            'email': email.value,
+            'senha': senha.value
+        }]
     }
-    )
+    else {
+        console.log("entrou no else")
+        novoUsuario = [{
+            'nome': nome.value,
+            'email': email.value,
+            'senha': senha.value
+        }]
+    }
+    localStorage.setItem('listaUser', JSON.stringify(novoUsuario))
 
-    localStorage.setItem('listarUser', JSON.stringify(listaUser))
 }
     
 
