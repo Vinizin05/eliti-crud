af59b57dff1eed3bf28141a2f0e7414847fb3f5a
Vinicius Rodrigues Varela <rodigues.vv@gmail.com>
2023-04-20 13:51:42 -0300

    atualizando o LS

--- JS/addUser.js
+++ JS/addUser.js
@@ -3,9 +3,9 @@ function cadastrar(){
     const nome = document.querySelector("#nome")
     const email = document.querySelector("#email")
     const senha = document.querySelector("#senha")
-    let listaUser = JSON.parse(localStorage.getItem('listaUser') && '[]') 
+    let listaUser = JSON.parse(localStorage.getItem('listaUser')) || []
 
-    listaUser.push ({
+    listaUser.push({
         nome: nome.value ,
         email: email.value ,
         senha: senha.value
