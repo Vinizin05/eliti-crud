3e78b30a23f872784b6b1dc88fd755a7dec1e2da
Patrick Henrique Medeiros Dos Santos <121632881+Patrick-Elitiano@users.noreply.github.com>
2023-04-20 01:40:11 -0300

    add js

--- JS/addUser.js
+++ JS/addUser.js
@@ -1,16 +1,22 @@
+const nome = document.querySelector("#nome")
+const email = document.querySelector("#email")
+const senha = document.querySelector("#senha")
+
 
 function cadastrar(){
-    const nome = document.querySelector("#nome")
-    const email = document.querySelector("#email")
-    const senha = document.querySelector("#senha")
-    let listaUser = JSON.parse(localStorage.getItem('listaUser') && '[]') 
-
-    listaUser.push ({
-        nome: nome.value ,
-        email: email.value ,
-        senha: senha.value
-    }
-    )
-
-    localStorage.setItem('listarUser', JSON.stringify(listaUser))
+    
+    console.log('kkkk')
+   let listaUser = JSON.parse(localStorage.getItem('listaUser') || []) 
+    
+        listaUser.push (
+            {
+            nomecad: nome.value ,
+            emailcad: email.value ,
+            senhacad: senha.value
+            }
+        )
+         
+     localStorage.setItem('listarUser', JSON.stringify(listaUser))
+     window.location.href = "/html/addUser.html"
+    
 }
