f64e14378bd5e140dc51d5a17d79c11783798362
Vinicius Rodrigues Varela <rodigues.vv@gmail.com>
2023-04-25 14:32:07 -0300

    .

--- JS/addUser.js
+++ JS/addUser.js
@@ -1,7 +1,3 @@
-// const nome = document.querySelector("#nome")
-// const email = document.querySelector("#email")
-// const senha = document.querySelector("#senha")
-
 window.onload = function (){
     exibirDados();
   }
